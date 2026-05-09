18b862cfa967e7916277091884dbbe29011c1327
Aleksandr Pavlov <ckidoz@gmail.com>
2026-05-09 20:05:11 +0300

    Split strlen into its own libc source file

--- libc/src/string.cpp
+++ libc/src/string.cpp
@@ -3,14 +3,6 @@
 
 extern "C" {
 
-size_t strlen(const char* s) {
-    size_t len = 0;
-    while (s[len]) {
-        len++;
-    }
-    return len;
-}
-
 char* strsignal(int sig) {
     (void)sig;
     return (char*)"Unknown signal";
---
+++ libc/src/strlen.cpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+#include <string.h>
+
+extern "C" {
+
+size_t strlen(const char* s) {
+    size_t len = 0;
+    while (s[len]) {
+        len++;
+    }
+    return len;
+}
+
+}
